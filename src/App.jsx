736d88500bf8f1a1330e55dafa4e2c984b8bb8d7
Josh Easley <easleyjs@gmail.com>
2023-10-22 02:02:15 -0700

    Adds working form and setup for tests

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,6 @@ import { useState } from 'react';
 
 import './App.scss';
 
-// Let's talk about using index.js and some other name in the component folder.
-// There's pros and cons for each way of doing this...
-// OFFICIALLY, we have chosen to use the Airbnb style guide naming convention. 
-// Why is this source of truth beneficial when spread across a global organization?
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Form from './Components/Form';
@@ -13,21 +9,14 @@ import Results from './Components/Results';
 
 function App() {
 
-  let [data, setData] = useState(null);
-  let [requestParams, setRequestParams] = useState({});
+  let [data, setData] = useState({});
+  let [requestParams, setRequestParams] = useState({ url: '', method: '' });
 
-  const callApi = (requestParams) => {
-    // mock output
-    const data = {
-      count: 2,
-      results: [
-        {name: 'fake thing 1', url: 'http://fakethings.com/1'},
-        {name: 'fake thing 2', url: 'http://fakethings.com/2'},
-      ],
-    };
+  const callApi = ( reqData ) => {
+    const { params, data } = reqData;
+ 
     setData(data);
-    setRequestParams(requestParams);
-    //this.setState({data, requestParams});
+    setRequestParams(params);
   }
 
   return (
--- src/Components/Form/index.jsx
+++ src/Components/Form/index.jsx
@@ -1,19 +1,60 @@
+import { useState } from 'react';
 import './Form.scss'
 
 function Form({ handleApiCall }) {
+    let [url, setUrl] = useState('');
+    let [method, setMethod] = useState('');
+
+    const handleUrlInput = (e) => {
+      e.preventDefault();
+      setUrl(e.target.value);
+    }
+
+    const handleMethodClick = (e) => {
+      e.preventDefault();
+      const selectedMethod = e.target.id;
+      const parentNode = e.target.parentNode;
+      
+      for (let i=0; i< parentNode.children.length; i++) {
+        let childNode = parentNode.children[i];
+        if ( childNode.id === selectedMethod ) {
+          childNode.className = "selectedMethod";
+        } else {
+          childNode.className = "";
+        }
+      }
+      setMethod(selectedMethod);
+    }
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+
+      const mockData = [{
+          site: 'Yahoo',
+          url: 'http://www.yahoo.com',
+          ranking: 50
+        },
+        {
+          site: 'Slashdot',
+          url: 'www.slashdot.org',
+          ranking: 1236432
+        }
+      ]
+      handleApiCall({ data: mockData, params: { url: url, method: method }});
+    }
 
     return (
-        <form onSubmit={ handleApiCall }>
+        <form id='form' onSubmit={ handleSubmit }>
         <label >
           <span>URL: </span>
-          <input name='url' type='text' />
-          <button type="submit">GO!</button>
+          <input data-testid="form-input" name='url' value={ url } type='text' onChange={ handleUrlInput } />
+          <button data-testid="form-button" type="submit">GO!</button>
         </label>
         <label className="methods">
-          <span id="get">GET</span>
-          <span id="post">POST</span>
-          <span id="put">PUT</span>
-          <span id="delete">DELETE</span>
+          <span id="get" onClick={ handleMethodClick }>GET</span>
+          <span id="post" onClick={ handleMethodClick }>POST</span>
+          <span id="put" onClick={ handleMethodClick }>PUT</span>
+          <span id="delete" onClick={ handleMethodClick }>DELETE</span>
         </label>
       </form>
     )
