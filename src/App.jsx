25a811b3f2f0f28de5b5257a5f779b701d47458d
Josh Easley <easleyjs@gmail.com>
2023-10-24 00:47:09 -0700

    Adds working useEffect and useReducer for results

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useReducer } from 'react';
 import axios from 'axios';
 
 import './App.scss';
@@ -9,27 +9,51 @@ import Form from './Components/Form';
 import Results from './Components/Results';
 
 function App() {
+  // need to replace this w/ reducer stuff
+  //let [resultsData, setResultsData] = useState({});
 
-  let [resultsData, setResultsData] = useState({});
-  let [requestParams, setRequestParams] = useState({ url: '', method: '' });
+  let initialState = {
+    url: '',
+    method: '',
+    data: null
+  }
+
+  const reqReducer = (state, action) => {
+    switch(action.type) {
+      case 'ADD_URL':
+        state.url = action.payload;
+      case 'ADD_METHOD':
+        state.method = action.payload; //might need to do a return after this. or break. not sure.
+      case 'ADD_DATA':
+        return { ...state, data: action.payload }
+      default:
+        return state
+    }
+  }
+
+  const [ state, dispatch ] = useReducer(reqReducer, initialState);
 
   useEffect(() => {
-    const callApi = async () => {
-      const results = await axios[requestParams.method](requestParams.url);
-      setResultsData(results);
+    const { url, method } = state;
+
+    if ( method && url ) {
+      axios[method](url).then((results) => {
+        dispatch({
+          type: 'ADD_DATA',
+          payload: results.data
+        })
+      })      
     }
-    if ( requestParams.method && requestParams.url ) {
-      callApi();
-    }
-  }, [requestParams])
+  }, [state.url, state.method]);
+
 
   return (
     <>
       <Header />
-      <div>Request Method: { requestParams.method }</div>
-      <div>URL: { requestParams.url }</div>
-      <Form updateReqParams={ setRequestParams } />
-      <Results data={ resultsData } />
+      <div>Request Method: { state.method }</div>
+      <div>URL: { state.url }</div>
+      <Form dispatchReqParams={ dispatch } />
+      <Results data={ state.data } />
       <Footer />
     </>
   );
--- src/Components/Results/index.jsx
+++ src/Components/Results/index.jsx
@@ -2,6 +2,7 @@ import './Results.scss'
 import JSONPretty from 'react-json-pretty';
 
 function Results(data) {
+    console.log('results:', data);
     return (
         <section>
             <JSONPretty id="json-pretty" data={ data }></JSONPretty>
